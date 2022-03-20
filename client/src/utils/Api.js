6885474df4e14d96d211c9ccce61a52c952baafb
Amit254742 <amit129181@gmail.com>
2022-03-20 21:13:24 +0545

    API url fixed

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,10 +1,8 @@
-import axios from 'axios'
-export default  axios.create({
-    baseURL: 'http://localhost:5000/api/',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-    })
-
-
+import axios from "axios";
+export default axios.create({
+  baseURL: "http://localhost:3035/api/",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    Authorization: "Bearer " + localStorage.getItem("token"),
+  },
+});
